a8896472eb3fcc3bf07b5737d17adaa7eed16854
nigue <anibaldavor@gmail.com>
2013-06-18 16:00:49 -0400

    esboso mock

---
+++ FlightTrace_mock/src/main/java/cl/median/trace/web/TestBeanMock.java
@@ -0,0 +1,32 @@
+package cl.median.trace.web;
+
+public class TestBeanMock {
+
+    private String nombre;
+    private int edad;
+    private boolean masculino;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public boolean isMasculino() {
+        return masculino;
+    }
+
+    public void setMasculino(boolean masculino) {
+        this.masculino = masculino;
+    }
+}
